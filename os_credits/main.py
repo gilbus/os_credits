83d567215fe3d3101f5740fca1949761b0566153
gilbus <coding@tinkershell.eu>
2019-01-30 17:17:49 +0100

    add logging config to config file

--- os_credits/main.py
+++ os_credits/main.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 from typing import Optional, List, TextIO
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, FileType
 from logging import getLogger
+from logging.config import dictConfig
 from os import getenv
 
 from aiohttp import web
 
-from os_credits.views import hello, print_it
+from os_credits.views import ping, influxdb_write_endpoint
 from os_credits.settings import default_config_path, load_config, config
 from os_credits.perun.requests import close_session
 
@@ -18,6 +19,8 @@ CONFIG_FILE_ENV_VAR = "CREDITS_SETTINGS_FILE"
 PORT_ENV_VAR = "CREDITS_PORT"
 HOST_ENV_VAR = "CREDITS_HOST"
 UNIX_DOMAIN_SOCKET_ENV_VAR = "CREDITS_UNIX_SOCKET"
+
+_logger = getLogger(__name__)
 
 
 def setup_app_internals_parser(parser: ArgumentParser) -> ArgumentParser:
@@ -31,8 +34,12 @@ async def create_app() -> web.Application:
     """
 
     app = web.Application()
-    app.add_routes([web.get(r"/{id}", hello), web.post("/write", print_it)])
+    app.add_routes(
+        [web.get(r"/ping", ping), web.post("/write", influxdb_write_endpoint)]
+    )
     app.update(name="os-credits", config=config)
+    if "logging" in config:
+        dictConfig(config["logging"])
 
     app.on_shutdown.append(close_session)
 
@@ -67,9 +74,9 @@ def main() -> int:
     parser.add_argument("-c", "--config", type=FileType(), default=default_config_path)
 
     args = parser.parse_args()
-    print(args)
 
     _logger = getLogger(__name__)
+    _logger.debug(args)
 
     try:
         web.run_app(create_app(), port=args.port, path=args.path, host=args.host)
