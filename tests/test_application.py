1b9b7978447a6006b77a0a55dad6e4796e47e0e5
gilbus <coding@tinkershell.eu>
2019-03-18 09:24:21 +0100

    modularize tests; use fixtures for env

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,27 +1,24 @@
+from pytest import fixture
+
 from os_credits.credits.measurements import Measurement
 
 
-async def test_startup(aiohttp_client, monkeypatch):
-    # must be available in the environment so the app is able to launch completely
-    monkeypatch.setenv("OS_CREDITS_PERUN_VO_ID", 0)
-    monkeypatch.setenv("OS_CREDITS_PERUN_LOGIN", 0)
-    monkeypatch.setenv("OS_CREDITS_PERUN_PASSWORD", 0)
+@fixture(name="credits_env")
+def fixture_credits_env(monkeypatch):
+    monkeypatch.setenv("OS_CREDITS_PERUN_VO_ID", "0")
+    monkeypatch.setenv("OS_CREDITS_PERUN_LOGIN", "0")
+    monkeypatch.setenv("OS_CREDITS_PERUN_PASSWORD", "0")
+    monkeypatch.setenv("INFLUXDB_HOST", "0")
+    monkeypatch.setenv("INFLUXDB_USER", "0")
+    monkeypatch.setenv("INFLUXDB_USER_PASSWORD", "0")
+    monkeypatch.setenv("INFLUXDB_DB", "0")
+
+
+async def test_settings(monkeypatch):
     monkeypatch.setenv("OS_CREDITS_PROJECT_WHITELIST", "ProjectA;ProjectB")
     monkeypatch.setenv("OS_CREDITS_PRECISION", "98")
-    monkeypatch.setenv("INFLUXDB_HOST", 0)
-    monkeypatch.setenv("INFLUXDB_USER", 0)
-    monkeypatch.setenv("INFLUXDB_USER_PASSWORD", 0)
-    monkeypatch.setenv("INFLUXDB_DB", 0)
-
-    from os_credits.main import create_app
     from os_credits.settings import config
 
-    app = await create_app()
-    client = await aiohttp_client(app)
-    resp = await client.get("/ping")
-    text = await resp.text()
-    assert resp.status == 200 and text == "Pong", "/ping endpoint failed"
-    # check for correct parsing and processing of settings via env vars
     assert config["OS_CREDITS_PROJECT_WHITELIST"] == {
         "ProjectA",
         "ProjectB",
@@ -29,6 +26,17 @@ async def test_startup(aiohttp_client, monkeypatch):
     assert (
         config["OS_CREDITS_PRECISION"] == 98
     ), "Integer value was not parsed/converted correctly from environment"
+
+
+async def test_startup(aiohttp_client, credits_env):
+    from os_credits.main import create_app
+
+    app = await create_app()
+    client = await aiohttp_client(app)
+    resp = await client.get("/ping")
+    text = await resp.text()
+    assert resp.status == 200 and text == "Pong", "/ping endpoint failed"
+    # check for correct parsing and processing of settings via env vars
 
     class _MeasurementA(
         Measurement, prometheus_name="measurement_a", friendly_name="measurement_a"
