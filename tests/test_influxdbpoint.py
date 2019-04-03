06a467fad148c0da4b3a9428d99547cad224e82f
gilbus <coding@tinkershell.eu>
2019-04-03 20:36:27 +0200

    Further InfluxDBPoint tests

--- tests/test_influxdbpoint.py
+++ tests/test_influxdbpoint.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
 from datetime import datetime
 
+from pytest import approx
+
 from os_credits.influxdb import InfluxDBPoint
 
 
@@ -12,7 +14,7 @@ class _TestPoint(InfluxDBPoint):
 
 def test_influx_line_conversion():
 
-    influx_line = "measurement,tag1=3,a=b value1=vkey1,b=c 1553342599293000000"
+    influx_line = b"measurement,tag1=3 value1=vkey1 1553342599293000000"
 
     point1 = _TestPoint.from_influx_line(influx_line)
     point2 = _TestPoint(
@@ -21,4 +23,11 @@ def test_influx_line_conversion():
         value1="vkey1",
         timestamp=datetime(2019, 3, 23, 13, 3, 19, 293000),
     )
-    assert point1 == point2
+    assert point1 == point2, "Parsing from Line Protocol failed"
+    # timestamp has to be compared separately
+    rest1, timestamp1 = point1.to_influxdb_line().decode().rsplit(" ", 1)
+    rest2, timestamp2 = influx_line.decode().rsplit(" ", 1)
+    # approx is necessary since we are losing some nanoseconds when converting
+    assert rest1 == rest2 and int(timestamp1) == approx(
+        int(timestamp2), 100
+    ), "Construction of Line Protocol failed"
